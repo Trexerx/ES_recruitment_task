249a029600a8f8855a4eb9fec0d97513d3189888
Trex <gruca.adam.pl@gmail.com>
2025-09-13 13:04:28 +0200

    0.0.1 - project setup, roadmap created

---
+++ main.py
@@ -0,0 +1,10 @@
+# AUTHOR Adam Gruca
+import settings as sett
+
+
+def create_new_point_names():
+    pass
+
+
+if __name__ == '__main__':
+    create_new_point_names()
---
+++ settings.py
@@ -0,0 +1,14 @@
+"""
+Adidtional settings if someone wants to use script for another dataset
+Author: Adam Gruca
+"""
+
+DATA_FILENAME = 'spatial_layers.gpkg'
+RESULTS_FILENAME = 'results.gpkg'
+
+LINE_LAYER_NAME = 'cieki'
+LINE_IDENTIFICATION_FIELD = 'oznaczenie'
+
+POINT_LAYER_NAME = 'punkty'
+POINT_OLD_NAME_FIELD = 'numer-stary'
+POINT_NEW_NAME_FIELD = 'numer-nowy'
