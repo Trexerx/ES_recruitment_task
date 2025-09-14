878b1f3cff37f9bb691703147852482729342497
Trex <gruca.adam.pl@gmail.com>
2025-09-14 04:04:24 +0200

    0.2.0 - segmenting points into groups intersecting with watercourses

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def create_results_file(old_filename: str | Path, new_filename: str | Path):
     copy2(old_filename, new_filename)
     assert Path(new_filename).exists(), 'Creating copy of spatial data failed'
 
+
 def open_spatial_layer(filename: str | Path, layer_name: str) -> QgsVectorLayer:
     """
     Opens specified layer from geospatial file
@@ -28,18 +29,18 @@ def open_spatial_layer(filename: str | Path, layer_name: str) -> QgsVectorLayer:
     assert opened_layer.isValid(), f"Cannot properly load layer {layer_name} from {filename}"
     return opened_layer
 
-
-def unique_values_from_field(layer: QgsVectorLayer, field_name: str):
-    """
-    Returns unique values of a field as a set.
-    :param layer:
-    :param field_name:
-    :return:
-    """
-    assert field_name in [f.name() for f in layer.fields()], f'No field named "{field_name}" in layer'
-    idx = layer.fields().indexOf(field_name)
-    unique_values = layer.uniqueValues(idx)
-    return unique_values
+# NOT USED FOR NOW, MAY BE DELETED IN LATER COMMITS
+# def unique_values_from_field(layer: QgsVectorLayer, field_name: str):
+#     """
+#     Returns unique values of a field as a set.
+#     :param layer:
+#     :param field_name:
+#     :return:
+#     """
+#     assert field_name in [f.name() for f in layer.fields()], f'No field named "{field_name}" in layer'
+#     idx = layer.fields().indexOf(field_name)
+#     unique_values = layer.uniqueValues(idx)
+#     return unique_values
 
 
 def prepare_new_values_dict(layer: QgsVectorLayer, old_name_field: str) -> dict:
@@ -85,10 +86,29 @@ def merge_lines_by_field_value(layer: QgsVectorLayer, field_name: str) -> dict[s
     return lines
 
 
+def intersecting_points_sorted_by_direction(point_layer: QgsVectorLayer, line: QgsGeometry) -> list[QgsGeometry]:
+    """
+    Searches for points intersecting with given line, and sorts them by distance from line start.
+    :param point_layer:
+    :param line:
+    :return:
+    """
+    assert QgsWkbTypes.geometryType(line.wkbType()) == QgsWkbTypes.LineGeometry, f'Line is not LineGeometry'
+    assert point_layer.geometryType() == QgsWkbTypes.PointGeometry, f'Points layer is not PointsGeometry'
+
+    intersecting_points = {}  # {distance_along_line: point_object}
+    for point in point_layer.getFeatures():
+        if line.distance(point.geometry()) < 1e-6:
+            intersecting_points[line.lineLocatePoint(point.geometry())] = point
+
+    sorted_intersecting_points = [point for distance, point in
+                                  dict(sorted(intersecting_points.items(), key=lambda item: item[0])).items()]
+
+    return sorted_intersecting_points
+
 
 # ============================================================================================================ MAIN ===
 def main():
-
     """
     In order to preserve original spatial data, the script creates a copy to work on
     """
@@ -104,13 +124,25 @@ def main():
     value and followed styling convention of other strings. Otherwise I would just leave it with no values.
     """
     points = open_spatial_layer(sett.RESULTS_FILENAME, sett.POINT_LAYER_NAME)
-    new_names_dict = prepare_new_values_dict(points, sett.POINT_OLD_NAME_FIELD)
+    new_names_dict = prepare_new_values_dict(points, sett.POINT_OLD_NAME_FIELD)  # FID: new_name
 
     """
     Watercourses are opened and merged into single lines by unique name in field sett.LINE_IDENTIFICATION_FIELD.
     """
     lines = open_spatial_layer(sett.RESULTS_FILENAME, sett.LINE_LAYER_NAME)
-    lines_by_name = merge_lines_by_field_value(lines, sett.LINE_IDENTIFICATION_FIELD)
+    lines_by_name = merge_lines_by_field_value(lines, sett.LINE_IDENTIFICATION_FIELD)  # water_name: QgsGeometry_obj
+
+    """
+    Each watercourse gets a list of points it is intersecting with.
+    Points are sorted by the distance along the line from the starting point.
+    
+    The intersection is realised by checking the distance between line and point, with 1e-6 threshold.
+    QgsGeometry.intersects() method gives unrealistic results due to float point rounding error.
+    """
+    sorted_points = {}  # water_name: list_of_points_sorted_by_distance
+    for name, line in lines_by_name.items():
+        sorted_p = intersecting_points_sorted_by_direction(points, line)
+        sorted_points[name] = sorted_p
 
 
 if __name__ == '__main__':
